13d99dbbe4beee8bc354eed47b3c04b928a1b172
Tasneemhasanat97 <tasneem.hasanat01@qmail.cuny.edu>
2023-10-23 14:56:06 -0400

    updated hamburger menu

--- src/components/UI/DrawerItems/index.tsx
+++ src/components/UI/DrawerItems/index.tsx
@@ -1,30 +1,138 @@
-import React from 'react';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Box, IconButton, Divider } from '@mui/material';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MovieIcon from '@mui/icons-material/Movie';
+import BookIcon from '@mui/icons-material/Book';
+import StarRateIcon from '@mui/icons-material/StarRate';
 
-import './styles/index.scss';
+const drawerWidth = 240;
 
 interface Props {
+	window?: () => Window;
 	toggleDrawer: () => void;
 }
 
-const DrawerItems = ({ toggleDrawer }: Props) => {
-	return (
-		<>
-			<Box sx={{ width: 300 }}>
-				<div className="drawer-header">
-					<IconButton onClick={toggleDrawer}>
-						<ChevronLeftIcon color="primary" />
-					</IconButton>
-				</div>
-				<Divider />
-				{/* 
-        TODO: add list items for different pages that the user can select
-        ref: https://mui.com/material-ui/react-drawer/
-        */}
-			</Box>
-		</>
-	);
-};
+export default function DrawerItems(props: Props) {
+	const { window } = props;
+	const [mobileOpen, setMobileOpen] = React.useState(false);
 
-export default DrawerItems;
+	const handleDrawerToggle = () => {
+		setMobileOpen(!mobileOpen);
+	};
+
+	const drawer = (
+		<div>
+			<Toolbar />
+			<Divider />
+			<List>
+				{['Movies', 'Manga', 'Anime', 'Top Rated'].map((text, index) => (
+					<ListItem key={text} disablePadding>
+						<ListItemButton>
+							<ListItemIcon>
+								{
+									[
+										<MovieIcon key="movies" />,
+										<BookIcon key="manga" />,
+										<MovieIcon key="anime" />,
+										<StarRateIcon key="top-rated" />,
+									][index]
+								}
+							</ListItemIcon>
+							<ListItemText primary={text} />
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
+
+	const container =
+		window !== undefined ? () => window().document.body : undefined;
+
+	return (
+		<Box sx={{ display: 'flex' }}>
+			<CssBaseline />
+			<AppBar position="fixed">
+				<Toolbar>
+					<IconButton
+						color="inherit"
+						aria-label="open drawer"
+						edge="start"
+						onClick={handleDrawerToggle}
+						sx={{ mr: 2, display: { sm: 'none' } }}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" noWrap component="div">
+						Review Hub
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Box
+				component="nav"
+				sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+				aria-label="review sections"
+			>
+				<Drawer
+					container={container}
+					variant="temporary"
+					open={mobileOpen}
+					onClose={handleDrawerToggle}
+					ModalProps={{
+						keepMounted: true,
+					}}
+					sx={{
+						display: { xs: 'block', sm: 'none' },
+						'& .MuiDrawer-paper': {
+							boxSizing: 'border-box',
+							width: drawerWidth,
+						},
+					}}
+				>
+					{drawer}
+				</Drawer>
+				<Drawer
+					variant="permanent"
+					sx={{
+						display: { xs: 'none', sm: 'block' },
+						'& .MuiDrawer-paper': {
+							boxSizing: 'border-box',
+							width: drawerWidth,
+							top: '64px', // or the height of your AppBar
+						},
+					}}
+					open
+				>
+					{drawer}
+				</Drawer>
+			</Box>
+			<Box
+				component="main"
+				sx={{
+					flexGrow: 1,
+					p: 3,
+					width: { sm: drawerWidth },
+				}}
+			>
+				<Toolbar />
+				{/* <Typography paragraph>
+					Welcome to the Review Hub! Browse through our movie, manga, and anime
+					reviews. Dive into the top-rated selections and discover something
+					new!
+				</Typography> */}
+			</Box>
+		</Box>
+	);
+}
