465e08d30c99e59cb371b46f5d8381f032552a5d
AndrewGa0 <andrew.gao51@qmail.cuny.edu>
2023-12-05 13:51:49 -0500

    Added Udemy Checkbox,RippleEffect + wave animation

--- src/components/UI/LoginForm/index.tsx
+++ src/components/UI/LoginForm/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Paper, Input, Button, Typography, Box, FormControlLabel, Checkbox } from '@mui/material';
+import {
+	Paper,
+	TextField,
+	Button,
+	Typography,
+	Box,
+	FormControlLabel,
+	Checkbox,
+} from '@mui/material';
+import WaveLabel from './WaveLabel';
+import './styles/index.scss';
 
 const LoginForm = () => {
 	return (
@@ -9,36 +19,72 @@ const LoginForm = () => {
 				justifyContent: 'center',
 				alignItems: 'center',
 				height: '100vh',
-			}}>
-			<Paper elevation={3} sx={{ padding: '40px', maxWidth: '400px', width: '100%', border: '1px solid white', backgroundColor: '#16151d' }}>
-				<Typography variant="h4" align="center" sx={{ marginBottom: '20px' }} gutterBottom>
+			}}
+		>
+			<Paper
+				elevation={3}
+				sx={{
+					padding: '40px',
+					maxWidth: '400px',
+					width: '100%',
+					border: '1px solid purple',
+					backgroundColor: '#16151d',
+				}}
+			>
+				<Typography
+					variant="h4"
+					align="center"
+					sx={{ marginBottom: '20px' }}
+					gutterBottom
+				>
 					User Login
 				</Typography>
-				<Input
-					sx={{
-						marginBottom: '40px',
-						color: 'white',
-						'&:before': {
-							borderBottom: '1px solid white',
-						},
-					}}
-					fullWidth
-					placeholder="Username or Email"
+				<div className="form-control">
+					<TextField
+						sx={{
+							marginBottom: '20px',
+							color: 'white',
+							'& .MuiInput-underline:before': {
+								borderBottom: '1px solid white',
+							},
+						}}
+						fullWidth
+						required
+						label=""
+						InputProps={{
+							endAdornment: <WaveLabel text="Email" />,
+						}}
+					/>
+				</div>
+				<div className="form-control">
+					<TextField
+						sx={{
+							marginBottom: '20px',
+							color: 'white',
+							'& .MuiInput-underline:before': {
+								borderBottom: '1px solid white',
+							},
+						}}
+						type="password"
+						fullWidth
+						required
+						label=""
+						InputProps={{
+							endAdornment: <WaveLabel text="Password" />,
+						}}
+					/>
+				</div>
+				<FormControlLabel
+					control={<Checkbox color="primary" />}
+					label="Remember me"
+					sx={{ marginTop: '10px' }}
 				/>
-				<Input
-					sx={{
-						marginBottom: '20px',
-						color: 'white',
-						'&:before': {
-							borderBottom: '1px solid white',
-						},
-					}}
-					type="password"
+				<Button
+					variant="contained"
+					color="primary"
 					fullWidth
-					placeholder="Password"
-				/>
-				<FormControlLabel control={<Checkbox color="primary" />} label="Remember me" sx={{ marginTop: '10px' }} />
-				<Button variant="contained" color="primary" fullWidth sx={{ marginTop: '20px' }}>
+					sx={{ marginTop: '20px' }}
+				>
 					Login
 				</Button>
 			</Paper>
